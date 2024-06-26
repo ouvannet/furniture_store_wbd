c05765159c1b93ad86d3c05aac4cccb51d0be07f
Kun Vath <matrixxteame@gmail.com>
2024-06-26 09:13:20 -0700

    add

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,54 @@
-</main>
-    <footer>
-        <p>&copy; 2024 Furniture Store. All rights reserved.</p>
-    </footer>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Footer</title>
+  <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+<footer>
+  <div class="row primary">
+    <div class="column about">
+      <h2>Foolish Developer</h2>
+      <ul>
+          <li><h3>Ou Vannet</h3></li>
+          <li><h3>Reasey Kunnvath</h3></li>
+          <li><h3>Pou Rathanak Sambath</h3></li>
+          <li><h3>Oueng Chhaiheng</h3></li>
+          <li><h3>Phan Khemara</h3></li>
+      </ul>
+    </div>
+    
+    <div class="column links">
+      <h2>Furniture Shop</h2>
+      <ul>
+          <li><a href="#shop">Shop</a></li>
+          <li><a href="#Delivery">Delivery Policy</a></li>
+          <li><a href="#Refund">Refund Policy</a></li>
+          <li><a href="#support">Support</a></li>
+      </ul>
+    </div>
+
+    <div class="column subscribe">
+      <h2>Stay Update!</h2>
+        <div>
+            <input type="email" placeholder="Your email" />
+            <button>Subscribe</button>
+        </div>
+    </div>
+  </div>
+</footer>
+
+<div class="row copyright">
+    <div class="footer-menu">
+        <a href="">Home</a>
+        <a href="">About</a>
+        <a href="">Contact</a>
+        <a href="">Blog</a>
+        <a href="">Social</a>
+    </div>
+    <p>Created by ST15. All right reserved 2024</p>
+  </div>
 </body>
 </html>
